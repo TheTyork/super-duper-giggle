2617b3ecf8561afc34aa702edd3075313f7f65bb
TheTyork <the.tyork1@gamil.com>
2022-02-10 20:16:35 +0300

    work models

--- DjangoProject/taskmanager/demoapp/admin.py
+++ DjangoProject/taskmanager/demoapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(ToDo)
+admin.site.register(page)
+admin.site.register(tab)
+admin.site.register(group)
+admin.site.register(param)
--- DjangoProject/taskmanager/demoapp/models.py
+++ DjangoProject/taskmanager/demoapp/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class ToDo(models.Model):
-    task = models.CharField(max_length=30)
-    discriptions = models.CharField(max_length=30)
+class page(models.Model):
+    title = models.CharField(max_length=30)
+
+class tab(models.Model):
+    title = models.CharField(max_length=30)
+    page = models.ForeignKey(page,on_delete=models.CASCADE)
+
+
+class group(models.Model):
+    title = models.CharField(max_length=30)
+    tab = models.ForeignKey(tab,on_delete=models.CASCADE)
+
+
+class param(models.Model):
+    name = models.CharField(max_length=30)
+    value = models.CharField(max_length=30)
+    si = models.CharField(max_length=30)
+    group = models.ForeignKey(group,on_delete=models.CASCADE)
