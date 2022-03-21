9cba33f6433137ee2ac95750a1c47696820321c1
TheTyork <the.tyork1@gmail.com>
2022-03-21 11:40:52 +0300

    обновление json работатет

--- DjangoProject/taskmanager/main/static/main/js/main.js
+++ DjangoProject/taskmanager/main/static/main/js/main.js
@@ -1,11 +1,9 @@
 async function jJson() {
     let response = await fetch('api/pages/')
-    return await response.json()
+    json = await response.json()
 }
 
 async function program() {
-    let json = await jJson()
-    console.log(json)
     let Pageflag = true
     let flagErrorTabs = true
     let FlagErrorGrups = true
@@ -18,14 +16,13 @@ async function program() {
         const TableParam = document.getElementById('table')
         let PageFor = document.createElement('li')
         let TextPageFor = document.createElement('a')
-        TextPageFor.className = ' nav-link  rounded-1 '
+        TextPageFor.className = ' nav-link  rounded-1'
         TextPageFor.setAttribute("data-bs-toggle", "pill")
         Pageflag = false
         MenuForPages.append(PageFor)
         TextPageFor.innerText = json[pages]['title']
         let PageTab = json[pages]['tab']
         PageFor.append(TextPageFor)
-
         TextPageFor.onclick = function () {
             let MenuGroups = document.getElementById('groups')
             MenuGroups.innerHTML = ''
@@ -56,85 +53,89 @@ async function program() {
                 MenuforTabs.append(TabFor)
                 TabFor.innerText = PageTab[tabs]['title']
                 let TabGroup = PageTab[tabs]['group']
-
                 TabFor.onclick = function () {
                     MenuGroups.innerHTML = ''
-                    for (let groups in TabGroup) {
-                        TableParam.innerText = ''
-                        let GroupFor = document.createElement("div")
-                        GroupFor.className = 'tab-pane fade show active p-2 h5'
-                        GroupFor.id = 'v-pills-' + PageTab[tabs]['id']
-                        GroupFor.setAttribute("aria-labelledby", 'v-pills-' + PageTab[tabs]['id'])
-                        GroupFor.innerText = TabGroup[groups]['title']
-                        MenuGroups.append(GroupFor)
-                        let GroupParam = TabGroup[groups]['param']
-                        let TableAll = document.createElement('table')
-                        MenuGroups.append(TableAll)
-                        TableAll.className = 'table table-dark border p-2'
-                        let TheadParam = document.createElement('thead')
-                        let TbodyParam = document.createElement('tbody')
-                        TableAll.append(TheadParam)
-                        TableAll.append(TbodyParam)
+                    console.log('sdaasd')
+                    setInterval(IntervalPages,1000)
+                    function IntervalPages() {
+                        MenuGroups.innerHTML = ''
+                        for (let groups in TabGroup) {
+                            TableParam.innerText = ''
+                            let GroupFor = document.createElement("div")
+                            GroupFor.className = 'tab-pane fade show active p-2 h5'
+                            GroupFor.id = 'v-pills-' + PageTab[tabs]['id']
+                            GroupFor.setAttribute("aria-labelledby", 'v-pills-' + PageTab[tabs]['id'])
+                            GroupFor.innerText = TabGroup[groups]['title']
+                            MenuGroups.append(GroupFor)
+                            let GroupParam = TabGroup[groups]['param']
+                            let TableAll = document.createElement('table')
+                            MenuGroups.append(TableAll)
+                            TableAll.className = 'table table-dark border p-2'
+                            let TheadParam = document.createElement('thead')
+                            let TbodyParam = document.createElement('tbody')
+                            TableAll.append(TheadParam)
+                            TableAll.append(TbodyParam)
 
-                        let TRHead = document.createElement('tr')
-                        TheadParam.append(TRHead)
-                        let TDParamName = document.createElement("td")
-                        TDParamName.innerText = 'Name'
-                        TRHead.append(TDParamName)
-                        let TDParamValue = document.createElement("td")
-                        TDParamValue.innerText = 'Value'
-                        TRHead.append(TDParamValue)
-                        let TDParamID = document.createElement("td")
-                        TDParamID.innerText = 'Si'
-                        TRHead.append(TDParamID)
-                        FlagErrorGrups = false
-                        let TRBodyError = document.createElement('tr')
-                        let TDError = document.createElement('td')
-                        TDError.innerText = "Данные отсутвуют"
-                        TRBodyError.append(TDError)
-                        TbodyParam.append(TRBodyError)
-                        TRBodyError.hidden = true
-                        for (let param in GroupParam) {
-                            let TRBody = document.createElement('tr')
-                            TbodyParam.append(TRBody)
-                            let ParamForName = document.createElement('td')
-                            ParamForName.innerText = GroupParam[param]['name']
-                            let ParamForValue = document.createElement('td')
-                            ParamForValue.innerText = GroupParam[param]['value']
-                            let ParamForSi = document.createElement('td')
-                            ParamForSi.innerText = GroupParam[param]['si']
-                            TRBody.append(ParamForName)
-                            TRBody.append(ParamForValue)
-                            TRBody.append(ParamForSi)
+                            let TRHead = document.createElement('tr')
+                            TheadParam.append(TRHead)
+                            let TDParamName = document.createElement("td")
+                            TDParamName.innerText = 'Name'
+                            TRHead.append(TDParamName)
+                            let TDParamValue = document.createElement("td")
+                            TDParamValue.innerText = 'Value'
+                            TRHead.append(TDParamValue)
+                            let TDParamID = document.createElement("td")
+                            TDParamID.innerText = 'Si'
+                            TRHead.append(TDParamID)
+                            FlagErrorGrups = false
+                            let TRBodyError = document.createElement('tr')
+                            let TDError = document.createElement('td')
+                            TDError.innerText = "Данные отсутвуют"
+                            TRBodyError.append(TDError)
+                            TbodyParam.append(TRBodyError)
+                            TRBodyError.hidden = true
+                            for (let param in GroupParam) {
+                                let TRBody = document.createElement('tr')
+                                TbodyParam.append(TRBody)
+                                let ParamForName = document.createElement('td')
+                                ParamForName.innerText = GroupParam[param]['name']
+                                let ParamForValue = document.createElement('td')
+                                ParamForValue.innerText = GroupParam[param]['value']
+                                let ParamForSi = document.createElement('td')
+                                ParamForSi.innerText = GroupParam[param]['si']
+                                TRBody.append(ParamForName)
+                                TRBody.append(ParamForValue)
+                                TRBody.append(ParamForSi)
+                            }
+                            if (TbodyParam.children.length === 1) {
+                                TRBodyError.hidden = false
+                            }
+
                         }
-                        if (TbodyParam.children.length === 1) {
-                            TRBodyError.hidden = false
+
+
+                        if (MenuforTabs.parentElement.offsetHeight > MenuGroups.parentElement.offsetHeight) {
+                            MenuforTabs.parentElement.className = 'p-3 border-end'
+                            MenuGroups.parentElement.className = "p-3"
+                        } else {
+                            MenuforTabs.parentElement.className = 'p-3'
+                            MenuGroups.parentElement.className = "p-3 border-start"
+                        }
+                        if (MenuGroups.children.length === 0) {
+                            MenuGroups.prepend(Error)
+                            Error.hidden = false
+
                         }
 
                     }
-
-                    if (MenuforTabs.parentElement.offsetHeight > MenuGroups.parentElement.offsetHeight) {
-                        MenuforTabs.parentElement.className = 'p-3 border-end'
-                        MenuGroups.parentElement.className = "p-3"
-                    } else {
-                        MenuforTabs.parentElement.className = 'p-3'
-                        MenuGroups.parentElement.className = "p-3 border-start"
-                    }
-                    if (MenuGroups.children.length === 0) {
-                        MenuGroups.prepend(Error)
-                        Error.hidden = false
-
-                    }
-
                 }
-
             }
             if (MenuforTabs.children.length === 0) {
                 (MenuforTabs.parentElement.parentElement.parentElement.firstElementChild).hidden = false
                 MenuforTabs.prepend(Error)
                 Error.hidden = false
                 MenuforTabs.parentElement.className = 'p-3'
-                MenuGroups.parentElement.className = "p-3   "
+                MenuGroups.parentElement.className = "p-3"
             }
         }
     }
@@ -142,8 +143,10 @@ async function program() {
 
 }
 
+let json = ''
 setInterval(jJson, 1000)
-setInterval(program, 10000)
-document.addEventListener('DOMContentLoaded', function () {
-    program()
-})
+setTimeout(program, 1500)
+
+
+
+
